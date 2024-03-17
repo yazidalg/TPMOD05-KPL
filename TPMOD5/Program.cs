eeafc10ab246032e3faa07d15ffb49962b6447cd
yazidalg <yazid.al2418@gmail.com>
2024-03-17 23:02:35 +0700

    add generic class

--- TPMOD5/Program.cs
+++ TPMOD5/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T> {
+    private T Data;
 
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah " + Data);
+    }
+}
+
+public class MainProgram
+{
+    public static void Main(String[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("130223047");
+        data.PrintData();
+    }
+}
